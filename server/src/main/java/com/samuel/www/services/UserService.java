1f9d6d61cc2bcac43cc06948d721477d293756aa
benisamuel <benisamuel566@gmail.com>
2025-11-23 12:08:13 +0200

    included use of exceptions

--- server/src/main/java/com/samuel/www/services/UserService.java
+++ server/src/main/java/com/samuel/www/services/UserService.java
@@ -1,9 +1,56 @@
 package com.samuel.www.services;
 
+import com.samuel.www.dtos.RegisterDto;
+import com.samuel.www.exceptions.BadRequestException;
+import com.samuel.www.exceptions.NotFoundException;
+import com.samuel.www.models.User;
 import com.samuel.www.repositories.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.UUID;
+
+@Service
 @AllArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+
+    public List<User> getAllUsers () {
+        return this.userRepository.findAll();
+    }
+
+    public User getUserById (UUID id) {
+        return this.userRepository.findById(id).orElseThrow(() -> new NotFoundException("User not found!!!"));
+    }
+
+    public User getUserByEmail (String email) {
+        return this.userRepository.getUserByEmail(email).orElseThrow(() -> new NotFoundException("User not found!!!"));
+    }
+
+    public User createUser (RegisterDto registerDto) {
+        boolean exist = this.userRepository.getUserByEmail(registerDto.getEmail()).isPresent();
+        if (exist) {
+            throw new BadRequestException("User with that email already exist!!!");
+        }
+        User newUser = new User(registerDto.getName(), registerDto.getEmail(), registerDto.getPassword(), registerDto.getRole(), registerDto.getJoined_at());
+        return this.userRepository.save(newUser);
+    }
+
+    public User updateUserById (UUID id, RegisterDto registerDto) {
+        User user = this.getUserById(id);
+
+        user.setEmail(registerDto.getEmail());
+        user.setName(registerDto.getName());
+        user.setPassword(registerDto.getPassword());
+        user.setRole(registerDto.getRole());
+
+        return this.userRepository.save(user);
+    }
+
+    public Boolean deleteUserById (UUID id) {
+        User user = this.getUserById(id);
+        userRepository.delete(user);
+        return true;
+    }
 }
