b9fbf6d4c6ef20f60ccdac97e0c3090da8428e10
benisamuel <benisamuel566@gmail.com>
2025-11-23 12:06:56 +0200

    include UUID id type

--- server/src/main/java/com/samuel/www/repositories/AccountTransactionRepository.java
+++ server/src/main/java/com/samuel/www/repositories/AccountTransactionRepository.java
@@ -3,5 +3,7 @@ package com.samuel.www.repositories;
 import com.samuel.www.models.AccountTransactions;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountTransactionRepository extends JpaRepository<AccountTransactions, Long> {
+import java.util.UUID;
+
+public interface AccountTransactionRepository extends JpaRepository<AccountTransactions, UUID> {
 }
