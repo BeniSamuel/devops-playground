85775375418ae35f75cfd11527dc563bcf556df4
benisamuel <benisamuel566@gmail.com>
2025-11-23 12:06:27 +0200

    Include UUID id type

--- server/src/main/java/com/samuel/www/repositories/AccountRepository.java
+++ server/src/main/java/com/samuel/www/repositories/AccountRepository.java
@@ -3,5 +3,7 @@ package com.samuel.www.repositories;
 import com.samuel.www.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository <Account, Long> {
+import java.util.UUID;
+
+public interface AccountRepository extends JpaRepository <Account, UUID> {
 }
