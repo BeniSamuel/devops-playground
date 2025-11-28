a6e01fccb03b15158990081ca553bc2fbcc6bdb6
benisamuel <benisamuel566@gmail.com>
2025-11-28 22:07:52 +0200

    added getting account by account type and user methods

--- server/src/main/java/com/samuel/www/repositories/AccountRepository.java
+++ server/src/main/java/com/samuel/www/repositories/AccountRepository.java
@@ -1,9 +1,14 @@
 package com.samuel.www.repositories;
 
+import com.samuel.www.enums.AccountType;
 import com.samuel.www.models.Account;
+import com.samuel.www.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface AccountRepository extends JpaRepository <Account, UUID> {
+    List<Account> getAccountsByAccountType (AccountType accountType);
+    List<Account> getAccountsByOwner(User owner);
 }
